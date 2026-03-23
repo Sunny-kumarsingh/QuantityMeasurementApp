93e31965e52f6f51b84f1f610fd6bf22269ff6be
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-03-23 10:58:36 +0530

    [sunny] Added uc17-springboot backend

---
+++ src/main/java/com/quantitymeasurement/quantitymeasurementapp/exception/UnsupportedOperationException.java
@@ -0,0 +1,13 @@
+package com.quantitymeasurement.quantitymeasurementapp.exception;
+
+public class UnsupportedOperationException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public UnsupportedOperationException(String msg) {
+		super(msg);
+	}
+}
